0ac50dde2da40cc93e7b7fa3f470986c7cec1923
mateus <mateusm.garcia0@gmail.com>
2021-10-08 21:22:33 -0300

    Translate function finished

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 import './main.scss'
+import { $ } from './vendor';
+import * as translation from './translation/translation';
+import * as comets from './comets/comets';
+
+
+$("#name").on('click', function(){
+    translation.translatePage("EN-US");
+})
+$(".gg-mouse").on('click', function(){
+    translation.translatePage("PT-BR");
+})
+
+comets.createComets(1);
